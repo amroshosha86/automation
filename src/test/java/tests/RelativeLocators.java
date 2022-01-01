75de446e5523efdb2f055f61f56c353e4d2c756c
amroshosha86 <amro.shosha86@gmail.com>
2022-01-01 18:42:33 +0200

    test

--- src/test/java/tests/RelativeLocators.java
+++ src/test/java/tests/RelativeLocators.java
@@ -3,10 +3,16 @@ package tests;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.indexeddb.model.Key;
 import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.functions.ExpectedCondition;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RelativeLocators {
@@ -15,27 +21,34 @@ public class RelativeLocators {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver(); 
 		
-		driver.get("https://opensource-demo.orangehrmlive.com/");
+		//open google search engine
+		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
+		
+		//implicitlyWait "wait until page to load"
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-
-		WebElement LoginText=driver.findElement(By.cssSelector("#logInPanelHeading"));
-				
-		WebElement LoginInput=
-		driver.findElement(RelativeLocator.with(By.tagName("input")).below(LoginText));
-		LoginInput.sendKeys("admin");
 		
+		//Search for "weather"
+		WebElement SearchInput=driver.findElement(By.name("q"));
+		SearchInput.sendKeys("weather"+ Keys.ENTER);
+		
+		//Explicit wait
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement Weatherlink=wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("weather")));
+		Weatherlink.click();
+		
+		//RelativeLocators
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
 		WebElement LoginButton=driver.findElement(By.id("btnLogin"));
-
+		
 		WebElement PasswordInput=
 		driver.findElement(RelativeLocator.with(By.tagName("input")).above(LoginButton));
 		PasswordInput.sendKeys("123");
 		
-		Thread.sleep(2000);
-		driver.quit();
+		//Thread.sleep(2000);
+		//driver.quit();
 	
 	}
 
